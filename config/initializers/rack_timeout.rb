2944b7875193ab48270b3eea45b30d09c9e0aa47
noirscape <github@notabug.nl>
2025-02-22 02:04:19 +0100

    fork: increase rack timeout
    it's private anyway and the default of 65 seconds doesn't work for my efforts

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,6 +1,6 @@
 # https://github.com/sharpstone/rack-timeout#configuring
 options = {
-  service_timeout: ENV.fetch("RACK_REQUEST_TIMEOUT", 65).to_i,
+  service_timeout: ENV.fetch("RACK_REQUEST_TIMEOUT", 3600).to_i,
   wait_timeout: false,
   wait_overtime: false,
   service_past_wait: false,
